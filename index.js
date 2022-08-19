2708ecee945879deab428f57c005fe33cff26fe3
Nashbi Grand-Jean <nhgrandjean1@gmail.com>
2022-08-18 23:47:51 -0400

    updates

--- index.js
+++ index.js
@@ -5,74 +5,25 @@ var inquirer = require('inquirer');
 
 //Questions for management 
 
-const databaseQuestions = () => { 
+const databaseQuestion = () => { 
     inquirer.prompt ( [
 
 {
     type: "list",
-    message: "What is the team manager's name?",
-    name: "Manager Name",
+    message: "What would you like to do?",
+    name: "Selecting a task",
     choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
 },  
-{
-    type: "input",
-    message: "What is the team manager's name?",
-    name: "Manager Name",
-}, 
-{
-    type: "input",
-    message: "What is the manager's ID?",
-    name: " Manager Employee ID",
-}, 
-{
-    type: "input",
-    message: "What is the manager's email address?",
-    name: "Manager Email Address",
-}, 
-{
-    type: "input",
-    message: "What is the manager's office number?",
-    name: " Manager Office number",
-}, 
-
 ]).then(data => {
-followUpQuestions();
-})
-};
 
-//Follow up questions for students 
-
-const followUpQuestions = () => {
-    inquirer.prompt (
-    [
-
-    {
-        type: "input",
-        message: "What is the employee's name?",
-        name: "Employee Email",
-    }, 
-
-    {
-        type: "input",
-        message: "What is the employee ID",
-        name: "Employee ID",
-    }, 
-
-    {
-        type: "input",
-        message: "What is the employee github?",
-        name: "Employee github",
-    }, 
-
-    {
-        type: "input",
-        message: "What is the team member's role",
-        name: "Employee role",
-    }, 
-
-]).then(data => {
-    followUpQuestions();
-
+    switch (key) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
 
     
 })};
@@ -82,4 +33,4 @@ const followUpQuestions = () => {
 
 
 
-databaseQuestions();
+databaseQuestion();
